33f86d7b6f69578fc1f8b925fddaee26d0fc6756
Nikita <nikitabecker2003@gmail.com>
2024-02-08 14:50:28 +0200

    adds user authintication and routes

--- model.py
+++ model.py
@@ -1,18 +1,22 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Configure your database URI
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100))
+
+    is_admin_user = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def set_admin(self):
+        self.is_admin_user = True
+
+    def check_password_hash(self, password):
+        return check_password_hash(self.password_hash, password)
